6b7747a10c952737c2e2a699c8e8cfce98e00fd9
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2022-04-25 18:45:40 +0300

    Fixed bug with datepickers

--- src/resources/js/views/pages/history/HistoryView.vue
+++ src/resources/js/views/pages/history/HistoryView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Datepicker from 'vue3-datepicker';
+import { lt } from 'date-fns/esm/locale';
 import { ref } from 'vue';
 
-const picked = ref(new Date());
+const dateFrom = ref(null);
+const dateTo = ref(null);
 </script>
 <template>
 	<div class="bg-white d-flex flex-column py-5 px-3 p-sm-5 col-12 col-xl-10 b-shadow">
@@ -12,20 +14,14 @@ const picked = ref(new Date());
 				<span class="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></span>
 				<input type="text" class="form-control bl-none" placeholder="Ieškoti pagal vardą">
 			</div>
-			<div class="date-wrapper d-flex gap-2 gap-md-0 w-md-50">
-				<div class="date-second-wrapper d-flex w-100">
-					<div class="input-group d-contents">
-						<span class="input-group-text bl-md-none"><i class="fa-solid fa-calendar-days"></i></span>
-						<Datepicker v-model="picked" class="form-control bl-none bg-white"/>
-						<!--					<input type="text" class="form-control bl-none" placeholder="Data nuo">-->
-					</div>
+			<div class="date-wrapper d-flex w-md-50">
+				<div class="input-group d-contents">
+					<span class="input-group-text bl-md-none"><i class="fa-solid fa-calendar-days"></i></span>
+					<Datepicker v-model="dateFrom" class="form-control bl-none bg-white" :locale="lt" placeholder="Data nuo"/>
 				</div>
-				<div class="date-second-wrapper d-flex w-100">
-					<div class="input-group d-contents">
-						<span class="input-group-text bl-md-none"><i class="fa-solid fa-calendar-days"></i></span>
-						<Datepicker v-model="picked" class="form-control bl-none bg-white"/>
-						<!--					<input type="text" class="form-control bl-none" placeholder="Data iki">-->
-					</div>
+				<div class="input-group d-contents">
+					<span class="input-group-text bl-none"><i class="fa-solid fa-calendar-days"></i></span>
+					<Datepicker v-model="dateTo" class="form-control bl-none bg-white" :locale="lt" placeholder="Data iki"/>
 				</div>
 			</div>
 		</div>
