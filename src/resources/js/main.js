d370dd735e309784b156dd2cc784552b3cc4b1a9
rpranaitis <90261967+rpranaitis@users.noreply.github.com>
2022-04-25 20:39:40 +0300

    Update main.js

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -3,11 +3,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-import Popper from 'vue3-popper';
-
 const app = createApp(App).use(createPinia()).use(router);
-
-app.component('popper-component', Popper);
 
 app.mount('#app');
 
